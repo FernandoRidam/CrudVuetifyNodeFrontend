3dfade1809d19e604b95242a2cc6e32c4cbbeec8
iotidev <iotidevelopment@gmail.com>
2019-08-29 09:49:39 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,64 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      :clipped="$vuetify.breakpoint.lgAndUp"
+    >
+      <v-list dense>
+        <template>
+          <v-list-item :to="{ name: 'home' }">
+            <v-list-item-action>
+              <v-icon> mdi-account </v-icon>
+            </v-list-item-action>
+            <v-list-item-title>
+              Users
+            </v-list-item-title>
+          </v-list-item>
+        </template>
+
+        <template>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon> mdi-settings </v-icon>
+            </v-list-item-action>
+            <v-list-item-title>
+              Settings
+            </v-list-item-title>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+        fluid
+        fill-height
+      >
+        <v-slide-y-transition mode="out-in">
+          <router-view />
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,170 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-layout align-start>
+    <v-flex>
+      <v-data-table
+        :headers="headers"
+        :items="users"
+        :search="search"
+        sort-by="calories"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>Categorias</v-toolbar-title>
+            <v-divider
+              class="mx-4"
+              inset
+              vertical
+            ></v-divider>
+            <div class="flex-grow-1"></div>
+            <v-text-field class="text-xs-center" v-model="search" append-icon="mdi-magnify" label="Buscar categoria" single-line hide-details> </v-text-field>
+            <div class="flex-grow-1"></div>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark class="mb-2" v-on="on">New User</v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
+  
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field v-model="editedItem.name" label="User Name"></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+  
+                <v-card-actions>
+                  <div class="flex-grow-1"></div>
+                  <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                  <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.action="{ item }">
+          <v-icon
+            small
+            class="mr-2"
+            @click="editItem(item)"
+          >
+            mdi-pencil
+          </v-icon>
+          <v-icon
+            small
+            @click="deleteItem(item)"
+          >
+            mdi-delete
+          </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize">Reset</v-btn>
+        </template>
+      </v-data-table>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import api from '../plugins/api';
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      return {
+        dialog: false,
+        search: '',
+        headers: [
+          {
+            text: 'Name',
+            align: 'center',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Actions', value: 'action', sortable: false },
+        ],
+        users: [],
+        editedIndex: -1,
+        editedItem: {
+          name: '',
+        },
+        defaultUser: {
+          name: '',
+        },
+      };
+    },
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New User' : 'Edit User'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      async initialize () {
+        this.users = []
+
+        const users = await api.get('/users');
+        
+        this.users = users.data;
+      },
+  
+      editItem (item) {
+        this.editedIndex = this.users.indexOf(item)
+        console.log( item );
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+  
+      deleteItem (item) {
+        const index = this.users.indexOf(item)
+        confirm('Are you sure you want to delete this item?')
+        && this.users.splice(index, 1)
+        && api.delete(`/user/${ item.id }`);
+      },
+  
+      close () {
+        this.dialog = false
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        }, 300)
+      },
+  
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.users[this.editedIndex], this.editedItem)
+          
+          const { name } = this.editedItem;
+
+          api.put(`/user/${ this.editedItem.id }`,{
+            name
+          });
+        } else {
+          this.users.push(this.editedItem)
+
+          const { name } = this.editedItem;
+          
+          api.post('/user', {
+            name
+          });
+        }
+        this.close()
+      },
+    },
+  };
 </script>
